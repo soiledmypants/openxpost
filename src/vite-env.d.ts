396568f711e303853f000215980e1b9447f080ed
soiledmypants <soilavandewege14@hotmail.com>
2026-08-13 19:07:54 +0000

    ship: src/vite-env.d.ts from docs branch

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -8,3 +8,7 @@ interface ImportMetaEnv {
 interface ImportMeta {
   readonly env: ImportMetaEnv;
 }
+
+declare module "virtual:source-files" {
+  export const files: { path: string; content: string }[];
+}
